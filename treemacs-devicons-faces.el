b307f6297a01fc3d987a4f9c14e84f4427351672
Daniel <rainstormstudio@yahoo.com>
2023-03-08 00:14:59 -0500

    add package docs

--- treemacs-devicons-faces.el
+++ treemacs-devicons-faces.el
@@ -1,3 +1,30 @@
+;;; treemacs-devicons-faces.el --- A module of faces for treemacs-devicons -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Hongyu Ding <rainstormstudio@yahoo.com>
+
+;; Author: Hongyu Ding <rainstormstudio@yahoo.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; treemacs-devicons-faces
+
+;;; Code:
+
 (defgroup treemacs-devicons-faces nil
   "Manage how devicons are colored and themed."
   :prefix "treemacs-devicons-"
@@ -8,204 +35,205 @@
 (defface treemacs-devicons-red
   '((((background dark)) :foreground "#AC4142")
     (((background light)) :foreground "#AC4142"))
-  "Face for red icons"
+  "Face for red icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lred
   '((((background dark)) :foreground "#EB595A")
     (((background light)) :foreground "#EB595A"))
-  "Face for lred icons"
+  "Face for lred icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dred
   '((((background dark)) :foreground "#843031")
     (((background light)) :foreground "#843031"))
-  "Face for dred icons"
+  "Face for dred icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-red-alt
   '((((background dark)) :foreground "#ce5643")
     (((background light)) :foreground "#843031"))
-  "Face for dred icons"
+  "Face for dred icons."
   :group 'treemacs-devicons-faces)
 
 ;; green
 (defface treemacs-devicons-green
   '((((background dark)) :foreground "#90A959")
     (((background light)) :foreground "#90A959"))
-  "Face for green icons"
+  "Face for green icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lgreen
   '((((background dark)) :foreground "#C6E87A")
     (((background light)) :foreground "#3D6837"))
-  "Face for lgreen icons"
+  "Face for lgreen icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dgreen
   '((((background dark)) :foreground "#6D8143")
     (((background light)) :foreground "#6D8143"))
-  "Face for dgreen icons"
+  "Face for dgreen icons."
   :group 'treemacs-devicons-faces)
 
 ;; yellow
 (defface treemacs-devicons-yellow
   '((((background dark)) :foreground "#FFD446")
     (((background light)) :foreground "#FFCC0E"))
-  "Face for yellow icons"
+  "Face for yellow icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lyellow
   '((((background dark)) :foreground "#FFC16D")
     (((background light)) :foreground "#FF9300"))
-  "Face for lyellow icons"
+  "Face for lyellow icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dyellow
   '((((background dark)) :foreground "#B48D56")
     (((background light)) :foreground "#B48D56"))
-  "Face for dyellow icons"
+  "Face for dyellow icons."
   :group 'treemacs-devicons-faces)
 
 ;; blue
 (defface treemacs-devicons-blue
   '((((background dark)) :foreground "#6A9FB5")
     (((background light)) :foreground "#6A9FB5"))
-  "Face for blue icons"
+  "Face for blue icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-blue-alt
   '((((background dark)) :foreground "#2188b6")
     (((background light)) :foreground "#2188b6"))
-  "Face for blue icons"
+  "Face for blue icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lblue
   '((((background dark)) :foreground "#8FD7F4")
     (((background light)) :foreground "#677174"))
-  "Face for lblue icons"
+  "Face for lblue icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dblue
   '((((background dark)) :foreground "#446674")
     (((background light)) :foreground "#446674"))
-  "Face for dblue icons"
+  "Face for dblue icons."
   :group 'treemacs-devicons-faces)
 
 ;; maroon
 (defface treemacs-devicons-maroon
   '((((background dark)) :foreground "#8F5536")
     (((background light)) :foreground "#8F5536"))
-  "Face for maroon icons"
+  "Face for maroon icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lmaroon
   '((((background dark)) :foreground "#CE7A4E")
     (((background light)) :foreground "#CE7A4E"))
-  "Face for lmaroon icons"
+  "Face for lmaroon icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dmaroon
   '((((background dark)) :foreground "#72584B")
     (((background light)) :foreground "#72584B"))
-  "Face for dmaroon icons"
+  "Face for dmaroon icons."
   :group 'treemacs-devicons-faces)
 
 ;; purple
 (defface treemacs-devicons-purple
   '((((background dark)) :foreground "#AA759F")
     (((background light)) :foreground "#68295B"))
-  "Face for purple icons"
+  "Face for purple icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-purple-alt
   '((((background dark)) :foreground "#5D54E1")
     (((background light)) :foreground "#5D54E1"))
-  "Face for purple icons"
+  "Face for purple icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lpurple
   '((((background dark)) :foreground "#E69DD6")
     (((background light)) :foreground "#E69DD6"))
-  "Face for lpurple icons"
+  "Face for lpurple icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dpurple
   '((((background dark)) :foreground "#694863")
     (((background light)) :foreground "#694863"))
-  "Face for dpurple icons"
+  "Face for dpurple icons."
   :group 'treemacs-devicons-faces)
 
 ;; orange
 (defface treemacs-devicons-orange
   '((((background dark)) :foreground "#D4843E")
     (((background light)) :foreground "#D4843E"))
-  "Face for orange icons"
+  "Face for orange icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lorange
   '((((background dark)) :foreground "#FFA500")
     (((background light)) :foreground "#FFA500"))
-  "Face for lorange icons"
+  "Face for lorange icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dorange
   '((((background dark)) :foreground "#915B2D")
     (((background light)) :foreground "#915B2D"))
-  "Face for dorange icons"
+  "Face for dorange icons."
   :group 'treemacs-devicons-faces)
 
 ;; cyan
 (defface treemacs-devicons-cyan
   '((((background dark)) :foreground "#75B5AA")
     (((background light)) :foreground "#75B5AA"))
-  "Face for cyan icons"
+  "Face for cyan icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-cyan-alt
   '((((background dark)) :foreground "#61dafb")
     (((background light)) :foreground "#0595bd"))
-  "Face for cyan icons"
+  "Face for cyan icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lcyan
   '((((background dark)) :foreground "#A5FDEC")
     (((background light)) :foreground "#2C7D6E"))
-  "Face for lcyan icons"
+  "Face for lcyan icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dcyan
   '((((background dark)) :foreground "#48746D")
     (((background light)) :foreground "#48746D"))
-  "Face for dcyan icons"
+  "Face for dcyan icons."
   :group 'treemacs-devicons-faces)
 
 ;; pink
 (defface treemacs-devicons-pink
   '((((background dark)) :foreground "#F2B4B8")
     (((background light)) :foreground "#FC505B"))
-  "Face for pink icons"
+  "Face for pink icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lpink
   '((((background dark)) :foreground "#FFBDC1")
     (((background light)) :foreground "#FF505B"))
-  "Face for lpink icons"
+  "Face for lpink icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dpink
   '((((background dark)) :foreground "#B18286")
     (((background light)) :foreground "#7E5D5F"))
-  "Face for dpink icons"
+  "Face for dpink icons."
   :group 'treemacs-devicons-faces)
 
 ;; silver
 (defface treemacs-devicons-silver
   '((((background dark)) :foreground "#716E68")
     (((background light)) :foreground "#716E68"))
-  "Face for silver icons"
+  "Face for silver icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-lsilver
   '((((background dark)) :foreground "#B9B6AA")
     (((background light)) :foreground "#7F7869"))
-  "Face for lsilver icons"
+  "Face for lsilver icons."
   :group 'treemacs-devicons-faces)
 (defface treemacs-devicons-dsilver
   '((((background dark)) :foreground "#838484")
     (((background light)) :foreground "#838484"))
-  "Face for dsilver icons"
+  "Face for dsilver icons."
   :group 'treemacs-devicons-faces)
 
 ;; white
 (defface treemacs-devicons-white
   '((((background dark)) :foreground "#ffffff")
     (((background light)) :foreground "#ffffff"))
-  "Face for white icons"
+  "Face for white icons."
   :group 'treemacs-devicons-faces)
 
 ;; black
 (defface treemacs-devicons-black
   '((((background dark)) :foreground "#000000")
     (((background light)) :foreground "#000000"))
-  "Face for black icons"
+  "Face for black icons."
   :group 'treemacs-devicons-faces)
 
 (provide 'treemacs-devicons-faces)
+;;; treemacs-devicons-faces.el ends here
